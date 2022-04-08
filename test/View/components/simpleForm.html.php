302abbd40a4435ff51aa33a1a825636cf9429eb2
NKOUANANG KEPSEU VIVIAN POREL <dev.vporel@gmail.com>
2022-04-08 20:48:05 +0200

    remove test folder

--- test/View/components/simpleForm.html.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <head>
-
-    </head>
-    <body>
-        <?= $form ?>
-    </body>
-</html>
--- test/public/css/sass/compile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-
-	set /P files= Noms des fichiers - sans extension - : 
-	for %%f in (%files%) do start sass %%f.scss ../%%f.css -w
-
