3e3fab639dcbbcbc11bdc29cf10773b1b09a111a
NKOUANANG KEPSEU VIVIAN POREL <dev.vporel@gmail.com>
2022-04-09 19:54:38 +0200

    add serviceConfiguration

---
+++ src/Core/Configuration/ServiceConfiguration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace VPFramework\Core\Configuration;
+
+class ServiceConfiguration{
+    private $services = null;
+
+    public function getService($name){
+        if($this->services === null){
+            $this->services = require ROOT."/config/services.php";
+        }
+        if($this->services !== null && array_key_exists($name, $this->services))
+            return $this->services[$name];
+        else 
+            throw new ServiceNotFoundException($name);
+    }
+}
